363874ca77aa28c1f70d55842db943988470c08e
Furkan YALVAC <furkanyalvac@icloud.com>
2022-09-19 19:49:39 +0300

    Update main.py

--- Positioning the Regulator on the Stream Cross Section/main.py
+++ Positioning the Regulator on the Stream Cross Section/main.py
@@ -1,8 +1,9 @@
 """
 Hello I am the Developer of This Code
 This Coding has been coded for Konya Technical University Civil Engineering Hydraulics Department.
-
-This code was developed by Hüseyin Furkan YALVAÇ on 26.05.2022
+In this code, the graph is formulated.
+The maximum, average and minimum water levels affecting the body were determined by trial and error method.
+This code was developed by Hüseyin Furkan YALVAÇ on 24.09.2022
 Advisor: Cihangir KÖYCEĞİZ
 """
 
@@ -21,7 +22,7 @@ while True:
         Cmax=2.18
     else:
        Cmax = ((-0.0210) * (pHo ** 6)) + ((0.2148) * (pHo ** 5)) - ((0.915) * (pHo ** 4)) + ((1.982) * (pHo ** 3)) - (
-                   (2.3081) * (pHo ** 2)) + ((1.414) * (pHo ** 1)) + (1.7719)  #Hocam Burada Denklemde Hata Olabilir Burayı Kontrol Etmekte Fayda Var
+                   (2.3081) * (pHo ** 2)) + ((1.414) * (pHo ** 1)) + (1.7719)  # There may be an error in the equation. It is useful to check here
 
 
     Ho1=float(Hsav/pHo)
@@ -32,7 +33,7 @@ while True:
 
 
         Hortma = round(random.uniform(0.01, 1.6), 2)
-        Cortma = (0.3 * (Hortma ** 3)) - (0.14 * (Hortma ** 2)) + (0.32 * Hortma) + 0.79 #Hocam Yine Aynı Şekilde Denklemi Kontrol Ederdek Daha İyi Olabilir
+        Cortma = (0.3 * (Hortma ** 3)) - (0.14 * (Hortma ** 2)) + (0.32 * Hortma) + 0.79
         Hort = Hortma * Ho1
         Cort = Cortma * Cmaxr
         Qort = Cort * Hsav * (Hort ** 1.5)
@@ -40,11 +41,11 @@ while True:
 
             Hminma = round(random.uniform(0.01, 1), 2)
             Cminma = (0.3 * (Hminma ** 3)) - (0.14 * (Hminma ** 2)) + (
-                    0.32 * Hminma) + 0.79  # Hocam Yine Aynı Şekilde Denklemi Kontrol Ederdek Daha İyi Olabilir
+                    0.32 * Hminma) + 0.79
             Hmin = Hminma * Ho1
             Cmin = Cminma * Cmaxr
             Qmin = Cmin * Hsav * (Hmin ** 1.5)
-            if ((Qmin5 - 0.8) <= Qmin <= (Qmin5 + 0.8)):#Hocam Bu kısımda Hassasiyeti Ayarlayabilriz Yine Yukarıdaki İf Komutlarıylada Ayarlayabiliriz Ama Hassasiyet Arttıkça Verileri Bulması Uzun Sürüyor
+            if ((Qmin5 - 0.8) <= Qmin <= (Qmin5 + 0.8)):# We can adjust the sensitivity in this section, we can also adjust it with the above if commands, but it takes longer to find the data as the sensitivity increases.
 
                 print("LÜTFEN VERİLERİ DÜZELTİNİZ HASSAİYET OLDUĞUNDAN DÜZELTME YAPILMALIDIR")
                 print(f"P/HO Değeriniz:{pHo} Seçıilmiştir.")
